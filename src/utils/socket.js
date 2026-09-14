2c6800d2c6cf593ec413bf26369e735fca5a6ba4
Rahul-S-Ashok <rahulashok2005@gmail.com>
2026-09-14 23:52:45 +0530

    fixed chat

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -13,7 +13,13 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
-  return io(BASE_URL, {
-    withCredentials: true,
-  });
+
+  if (location.hostname === "localhost") {
+
+    return io(BASE_URL, { withCredentials: true });
+
+  }
+
+  return io("/", { path: "/api/socket.io", withCredentials: true });
+
 };
