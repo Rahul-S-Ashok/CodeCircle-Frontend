3260ffc4db783ca581fe55e76437cafd66e0520b
Rahul-S-Ashok <rahulashok2005@gmail.com>
2026-09-14 21:22:10 +0530

    proxy changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
-// export const BASE_URL =
-//   location.hostname === "localhost" ? "http://localhost:8080" : "/api";
-
-// export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "";
-
 export const BASE_URL =
-  import.meta.env.VITE_BASE_URL || "http://localhost:8080";
+  import.meta.env.VITE_BASE_URL || "/api";
 
 export const GOOGLE_CLIENT_ID =
   import.meta.env.VITE_GOOGLE_CLIENT_ID || "";
+
+  // export const BASE_URL =
+//   location.hostname === "localhost" ? "http://localhost:8080" : "/api";
+
+// export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "";
